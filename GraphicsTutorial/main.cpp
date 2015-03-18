61eab2bf0072ab175cfb7f5cb65b5bdd13a6d925
Lalo <lalo.sa.94@gmail.com>
2015-03-18 12:25:50 -0600

    Initialized repo

---
+++ GraphicsTutorial/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  GraphicsTutorial
+//
+//  Created by Eduardo Alberto Sanchez Alvarado on 3/1/15.
+//  Copyright (c) 2015 Eduardo Alberto Sanchez Alvarado. All rights reserved.
+//
+
+#include "MainGame.h"
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    MainGame mainGame;
+    mainGame.run();
+    
+    return 0;
+}
